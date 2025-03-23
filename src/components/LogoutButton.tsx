cdb48dc36ef5371ad7c4b7eb84912212531a4bb3
mecastor <mecastor87@gmail.com>
2025-03-23 15:55:36 +0000

    wip

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,14 +1,28 @@
 "use client";
 
 import { useState } from "react";
-import { Button } from "./ui/button";
 import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+import { Button } from "./ui/button";
 
 const LogoutButton = () => {
+  const router = useRouter();
+
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleLogout = () => {
+    setIsLoading(true);
     console.log("logging out");
+
+    try {
+      router.push("/");
+      toast.success("Logged out");
+    } catch (err) {
+      console.log(err);
+      toast.error("Failed to log out");
+    }
   };
 
   return (
